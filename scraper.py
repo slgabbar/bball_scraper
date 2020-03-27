e9e48a32d75de0fecc6df8263f5b1f7a3d6dbcac
Sam Gabbard <samgabbard@Sams-MacBook-Pro.local>
2020-03-27 12:26:46 -0700

    automated stats

--- scraper.py
+++ scraper.py
@@ -2,28 +2,49 @@
 import os, errno
 from selenium import webdriver
 
-# Specify Driver and load URL
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-driver.get('https://www.basketball-reference.com/leagues/NBA_2018_per_game.html')
+def scrape_bball_stats(type, year_start, year_end, savedir, filename):
+    temp_url = "https://www.basketball-reference.com/leagues/NBA_{season}_{type}.html"
+    for year in range(year_start, year_end+1):
 
-# Create the directory to put data
-PATH = 'data/'
-try:
-    os.makedirs(path)
-except OSError as e:
-    if e.errno != errno.EEXIST:
-        raise
+        # define the url from season and type
+        url = temp_url.format(season=year, type=type)
 
-# Gets the raw csv content from bballref and stores in raw_csv as a string
-raw_csv = driver.execute_script('''var x = document.getElementsByClassName("tooltip");
-       x[3].click();
-       var content = document.getElementById("csv_per_game_stats");
-       return content.textContent;
-       ''')
+        # Load the url
+        driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+        driver.get(url)
 
-# Write CSV todisk
-outfile = "2018_pergame.csv"
-f = open(PATH+outfile, "w")
-f.write(raw_csv)
+        # define some variable to help with scraping
+        partial_table = '{type}_stats_toggle_partial_table'.format(type=type)
+        csv_content = 'csv_{type}_stats'.format(type=type)
 
-print("Done.")
+        # Hide partial rows so players who have played for multiple teams in one season
+        # do not show up twice.
+        driver.execute_script('document.getElementById("'+ partial_table +'").click();')
+
+        # Gets the raw csv content from bballref and stores in raw_csv as a string
+        raw_csv = driver.execute_script('''var x = document.getElementsByClassName("tooltip");
+               x[3].click();
+               var content = document.getElementById("'''+csv_content+'''");
+               return content.textContent;
+               ''')
+
+        # Create dir if not already there
+        # PATH = 'data/{season}_season/'.format(season=year)
+        PATH = savedir
+        try:
+            os.makedirs(PATH)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+
+        # Write CSV todisk
+        # filename = "{season}_{type}.csv".format(season=year, type=type)
+        f = open(PATH+filename, "w")
+        f.write(raw_csv)
+
+        print("Finished {season}_{type}".format(season=year, type=type))
+
+scrape_bball_stats('per_game', 1990, 1990, 'data/', '1990_per_game.csv')
+scrape_bball_stats('advanced', 1990, 1990, 'data/', '1990_advanced.csv')
+scrape_bball_stats('per_minute', 1990, 1990, 'data/', '1990_per_min.csv')
